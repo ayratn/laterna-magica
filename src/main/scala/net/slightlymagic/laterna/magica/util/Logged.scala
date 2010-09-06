e614c3a6f68541e2822816034df1a6b3a80ae7da
Silly Freak <clemens.koza@gmx.at>
2010-09-06 16:12:02 +0000

    Corrected an error in TreeProperties that made it windows-incompatible -used Matcher.quoteReplacement() to preserve backslashes in variables such as ${user.home} -used File.isAbsolute instead of String.startsWith("/")

--- src/main/scala/net/slightlymagic/laterna/magica/util/Logged.scala
+++
@@ -1,7 +0,0 @@
-package net.slightlymagic.laterna.magica.util
-
-import org.slf4j._
-
-trait Logged {
-    val log = LoggerFactory.getLogger(getClass())
-}
--- src/main/scala/net/slightlymagic/laterna/magica/util/MagicaFunctions.scala
+++
@@ -1,8 +0,0 @@
-package net.slightlymagic.laterna.magica.util
-
-import net.slightlymagic.laterna.magica.MagicObject
-import net.slightlymagic.laterna.magica.player.Player
-
-object MagicaFunction {
-  def controller() = (c: MagicObject) => c.getController()
-}
